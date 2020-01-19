9f736ee570ad6772a2b5c1054ea90a8ae75f824d
amiirbazzii <amir.hossein.b.a1011@gmail.com>
2020-01-18 17:50:16 -0800

    add redux directory to project

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -30,7 +30,7 @@ const Header = ({ currentUser,hidden }) => (
                 currentUser ?(
                 <div className='option' onClick={() => auth.signOut()} > SIGN OUT </div>
                 ):(
-                <Link className='option' to='/signin'> SIGN IN </Link>
+                <Link className='option' to='/shop'> SIGN IN </Link>
                 )
             }
             <CartIcon/>
