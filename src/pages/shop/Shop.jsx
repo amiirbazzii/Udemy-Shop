67a023bda50b85954407b0426c07bb62a07607ea
amiirbazzii <amir.hossein.b.a1011@gmail.com>
2020-01-20 22:22:49 -0800

    fix collection page error but shop page have error

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -8,7 +8,7 @@ const Shop = ({ match }) =>  {
    return (
         <div className="shop-page">
             <Route exact path={`${match.path}`} component={CollectionOverview} />
-            <Route exact path={`${match.path}/:CollectionId`} component={Collection}/>
+            <Route exact path={`${match.path}/:collectionId`} component={Collection}/>
         </div>
     )
 };
